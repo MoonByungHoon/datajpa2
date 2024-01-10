f6db6a16d07bdfb4128163ffb586f195a4988022
MoonByungHoon <mbh1069@naver.com>
2024-01-10 13:56:31 +0900

    2024-01-10

---
+++ src/main/java/jpabasic/data_jpa_2/DataJpa2Application.java
@@ -0,0 +1,13 @@
+package jpabasic.data_jpa_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/jpabasic/data_jpa_2/DataJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.data_jpa_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
